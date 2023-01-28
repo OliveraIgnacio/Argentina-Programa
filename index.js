d8bafbe1ce62e179f36a607a7d6919fd5399dc1e
Olivera Ignacio <oliveraignaciook@gmail.com>
2023-01-27 22:05:21 -0300

    finish js

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById('button_experience').addEventListener('click', ()=> {
+    document.getElementById('work').style.display = 'block'; 
+});
+
+
+document.getElementById('button_education').addEventListener('click', ()=> {
+    document.getElementById('education').style.display = 'block';    
+});
+
+
+document.getElementById('button_projects').addEventListener('click', ()=> {
+    document.getElementById('projects').style.display = 'block';    
+});
